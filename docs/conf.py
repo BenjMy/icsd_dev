ad68d48093797c7f1e26e98be3e9ec8d047dbab3
BenjiM <benjaminmary.etu@gmail.com>
2020-04-22 16:53:51 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ project = 'ICSD'
 copyright = '2020, Benjamin Mary'
 author = 'L. Peruzzo and B. Mary'
 # The short X.Y version
-version = ''
+version = '0.1.0'
 # The full version, including alpha/beta/rc tags
 release = ''
 
