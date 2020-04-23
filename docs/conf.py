6cb64c36e07568a4b6a7ca1c64b4a0c2f089f886
BenjiM <benjaminmary.etu@gmail.com>
2020-04-23 19:45:57 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../src'))
-sys.path.append(os.path.relpath('../src/icsd3d'))
-sys.path.append(os.path.abspath('..{}'.format(os.path.sep)))
+sys.path.append(os.path.relpath('../src/icsd3d/'))
+#sys.path.append(os.path.abspath('..{}'.format(os.path.sep)))
 from icsd3d_class import iCSD3d_Class
 
 
