e7973a0b5196965223525137e1814bae61508385
BenjiM <benjaminmary.etu@gmail.com>
2020-04-30 12:29:00 +0200

    new path

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -16,7 +16,7 @@ import os
 
 # path2files = os.path.join(os.path.dirname('__file__'), '/Cotton_Soil/Nail1/')
 print(os.getcwd())
-path2files= './Cotton_Soil/Nail1/'
+path2files= '/Cotton_Soil/Nail1/'
 
 # path2files = 
 
