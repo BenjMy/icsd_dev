38bdc3aeddd7a0a2788f87bc2f3be3cefc721de3
BenjiM <benjaminmary.etu@gmail.com>
2020-10-21 10:58:24 +0200

    add class to gridder

--- icsd3d/gridder/__init__.py
+++ icsd3d/gridder/__init__.py
@@ -1,15 +1,4 @@
 """
-Read utilities.
-
-Wrappers to facilitate import of common ERT data
-
-* :mod:`~icsd3d.importers.gimli`
-* :mod:`~icsd3d.importers.resipy`
-
-.. gimli: https://www.pygimli.org/
-.. resipy: https://gitlab.com/hkex/pyr2
-
-----
-
+Gridder utilities
 """
 from gridder.mkgrid import mkGrid_XI_YI 
