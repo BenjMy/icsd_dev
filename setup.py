011de7ca64f2fc0ec3d0ee822a41c3335819aff3
BenjMy <36472081+BenjMy@users.noreply.github.com>
2022-05-09 15:05:47 +0200

    add setupy.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("Readme.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="icsd_dev",
+    version="0.0.1",
+    author="B. Mary",
+    author_email="bmary@lbl.gov",
+    description="Python package for managing icsd processing, and visualization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BenjMy/icsd_dev",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License " + "v3 (LGPLv3)",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=["numpy", "pandas", "pyvista"]
+) 
